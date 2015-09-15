e2d35c415eaf14a9f4b8b6102ad231c0885ce4e9
yjhcfzx <andyzheng84@outlook.com>
2015-09-15 19:35:23 +0800

    ckeditor

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -18,13 +18,33 @@ class Welcome extends My_Controller {
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
-	{
+	{	
+		$path = //'../js/ckfinder';
+		$this->config->item( 'base_theme_url') . 'js/ckfinder/';
+		$width = '850px';
+		$this->editor($path, $width);
+		
+		
 		$this->load->view('templates/header',
 				$this->data
 		);
 		
 		$this->load->view('welcome_message');
 		$this->load->view('templates/footer', $this->data);
+	}
+	
+	function editor($path,$width) {
+		//Loading Library For Ckeditor
+		$this->load->library('ckeditor');
+		$this->load->library('ckFinder');
+		//configure base path of ckeditor folder
+		$this->ckeditor->basePath = $this->config->item( 'base_theme_url') . 'js/ckeditor/';
+		//base_url().'js/ckeditor/';
+		$this->ckeditor-> config['toolbar'] = 'Full';
+		$this->ckeditor->config['language'] = 'en';
+		$this->ckeditor-> config['width'] = $width;
+		//configure ckfinder with ckeditor config
+		$this->ckfinder->SetupCKEditor($this->ckeditor,$path);
 	}
 }
 
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,1 +1,7 @@
-welcome
+<div id="container">
+  <h1>Welcome to CodeIgniter!</h1>
+   <?php echo $this->ckeditor->editor('description',@$default_value);?> 
+  <input type="submit" name="submit" value="Save" id="save" class="save" />
+
+  <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
