7425e1615ff406a9a5c441448aa9b67aac108947
yjhcfzx <andyzheng84@126.com>
2016-01-12 14:42:22 +0800

    Slider

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,3 +1,62 @@
+ <style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 100%;
+      max-height:820px;
+      margin: auto;
+  }
+  .carousel-control{width:20%;}
+.glyphicon-chevron-right{right:15% !important;}
+.glyphicon-chevron-left{left:15% !important;}
+  </style>
 <div id="container">
-  <h1>Welcome</h1>
+    
+  <?php if(isset($recent_hot_post)):
+        $indicator = '<ol class="carousel-indicators">';
+    $inner = '<div class="carousel-inner" role="listbox">';?> 
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        
+    <?php
+    $index = 0;
+    foreach($recent_hot_post as $p):
+    $imgOfText = NULL;
+            if($p['main_image']){
+                $imgOfText = $this->config->item( 'base_upload_url')  . $p['main_image'];
+            }
+              else  if($p['content']){
+                $doc = new DOMDocument();
+                $doc->loadHTML($p['content']);
+                $img = $doc->getElementsByTagName('img')->item(0);
+                if($img){$imgOfText = $img->getAttribute('src');}}
+                if($imgOfText){
+                    $indicator .= ' <li data-target="#myCarousel" data-slide-to="' . $index . '" ' . (($index == 0) ? ' class="active"' : '') . '></li>';
+                    $inner .= ' <div class="item ' . (($index == 0) ? ' active' : '') . '">
+      <img src="' . $imgOfText . '" alt="img">
+      <div class="carousel-caption">
+        <h3>' . $p['title'] . '</h3>
+      </div>
+    </div>';
+                    $index++;
+                }
+                
+    ?>
+<?php
+    endforeach;
+    $indicator .= '</ol>';
+    $inner .= '</div>';
+     echo $indicator,$inner ;
+    ?>
+          <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    <?php
+endif;?>
+
 </div>
